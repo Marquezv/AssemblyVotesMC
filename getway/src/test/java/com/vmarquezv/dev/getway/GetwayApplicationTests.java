db220db8007e3666f301a5abf5446767d22e3b11
Marquezv <vmarquezv@outlook.com.br>
2023-01-07 20:18:01 -0300

    :construction: Getway - Created for managing APIs(User, Session) :fix: User, Session - changed ports

---
+++ getway/src/test/java/com/vmarquezv/dev/getway/GetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmarquezv.dev.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
